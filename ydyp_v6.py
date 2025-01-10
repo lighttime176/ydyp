c7dd4b62a02020cc81e65232640318f0b138b68a
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2025-01-10 10:09:51 +0800

    Update ydyp_v6.py

--- ydyp_v6.py
+++ ydyp_v6.py
@@ -16,7 +16,7 @@ import re
 import time
 from os import path
 import requests
-import schedule
+
 
 ua = 'Mozilla/5.0 (Linux; Android 11; M2012K10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36 MCloudApp/10.0.1'
 
